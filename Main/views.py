2cd3344ea85d3365a7a49e289c15ddc82f51876f
Juan Pablo Fonte <jpfontecv@gmail.com>
2026-03-16 22:14:43 -0300

    Feat: Unidad 12

--- Main/views.py
+++ Main/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from .forms import EditUserForm, AvatarForm
+from .models import Avatar
 
 
 def index(request):
     return render(request, "Main/index.html")
+
+
+def perfil(request):
+    return render(request, "Main/perfil.html")
+
+
+@login_required
+def editar_perfil(request):
+    if request.method == "POST":
+        form = EditUserForm(request.POST, instance=request.user)
+        try:
+            avatar = request.user.avatar
+        except Avatar.DoesNotExist:
+            avatar = None
+        if avatar:
+            avatar_form = AvatarForm(request.POST, request.FILES, instance=avatar)
+        else:
+            avatar_form = AvatarForm(request.POST, request.FILES)
+
+        if form.is_valid() and avatar_form.is_valid():
+            form.save()
+            avatar_instance = avatar_form.save(commit=False)
+            avatar_instance.user = request.user
+            avatar_instance.save()
+            return redirect("Main:perfil")
+    else:
+        form = EditUserForm(instance=request.user)
+        if hasattr(request.user, "avatar"):
+            avatar_form = AvatarForm(instance=request.user.avatar)
+        else:
+            avatar_form = AvatarForm()
+    return render(
+        request, "Main/editar_perfil.html", {"form": form, "avatar_form": avatar_form}
+    )
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,14 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
+from django.views.generic import (
+    ListView,
+    DetailView,
+    DeleteView,
+    CreateView,
+    UpdateView,
+)
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
 
@@ -10,6 +20,19 @@ def post_list(request):
     else:
         post_list = Post.objects.all()
     return render(request, "blog/post_list.html", context={"posts": post_list})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        busqueda = self.request.GET.get("busqueda", None)
+        if busqueda:
+            queryset = queryset.filter(titulo__icontains=busqueda)
+        return queryset
 
 
 def post_create(request):
@@ -26,3 +49,32 @@ def post_create(request):
     else:
         form = PostForm()
     return render(request, "blog/post_create.html", context={"form": form})
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy("blog:post_list")
+
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            form.instance.autor = self.request.user
+        else:
+            form.add_error(None, "Debes estar logueado para crear una publicación.")
+            return self.form_invalid(form)
+        return super().form_valid(form)
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy("blog:post_list")
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy("blog:post_list")
